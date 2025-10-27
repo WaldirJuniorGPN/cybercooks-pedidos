c80d1c0ae617effb820357bddad22b4d5a906f4b
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-10-27 16:52:08 -0300

    feat: implementa xceção personalizada para validar regras de negócio

---
+++ src/main/java/br/com/cybercooks/pedidos/exception/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package br.com.cybercooks.pedidos.exception;
+
+public class RegraDeNegocioException extends RuntimeException {
+
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
